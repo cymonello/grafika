20c75593ba5c5350a6a4ead7c11cdf7037eb6b2c
Szymon <szymek26@outlook.com>
2017-01-03 22:07:19 +0100

    wstepne gui

--- Grafika/Grafika/Form1.Designer.cs
+++ Grafika/Grafika/Form1.Designer.cs
@@ -28,12 +28,200 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.funcRadioButton1 = new System.Windows.Forms.RadioButton();
+            this.funcRadioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.funcRadioButton3 = new System.Windows.Forms.RadioButton();
+            this.funcRadioButton4 = new System.Windows.Forms.RadioButton();
+            this.timeTrackBar = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startAnimationButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.stopAnimationButton = new System.Windows.Forms.Button();
+            this.saveBmpButton = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.timeTrackBar)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Location = new System.Drawing.Point(266, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(749, 341);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Location = new System.Drawing.Point(266, 359);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(749, 376);
+            this.panel2.TabIndex = 0;
+            // 
+            // funcRadioButton1
+            // 
+            this.funcRadioButton1.AutoSize = true;
+            this.funcRadioButton1.Location = new System.Drawing.Point(6, 37);
+            this.funcRadioButton1.Name = "funcRadioButton1";
+            this.funcRadioButton1.Size = new System.Drawing.Size(72, 17);
+            this.funcRadioButton1.TabIndex = 1;
+            this.funcRadioButton1.TabStop = true;
+            this.funcRadioButton1.Text = "Funkcja 1";
+            this.funcRadioButton1.UseVisualStyleBackColor = true;
+            // 
+            // funcRadioButton2
+            // 
+            this.funcRadioButton2.AutoSize = true;
+            this.funcRadioButton2.Location = new System.Drawing.Point(6, 60);
+            this.funcRadioButton2.Name = "funcRadioButton2";
+            this.funcRadioButton2.Size = new System.Drawing.Size(72, 17);
+            this.funcRadioButton2.TabIndex = 2;
+            this.funcRadioButton2.TabStop = true;
+            this.funcRadioButton2.Text = "Funkcja 2";
+            this.funcRadioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Wybierz funkcję opisującą teren:";
+            // 
+            // funcRadioButton3
+            // 
+            this.funcRadioButton3.AutoSize = true;
+            this.funcRadioButton3.Location = new System.Drawing.Point(6, 83);
+            this.funcRadioButton3.Name = "funcRadioButton3";
+            this.funcRadioButton3.Size = new System.Drawing.Size(72, 17);
+            this.funcRadioButton3.TabIndex = 4;
+            this.funcRadioButton3.TabStop = true;
+            this.funcRadioButton3.Text = "Funkcja 3";
+            this.funcRadioButton3.UseVisualStyleBackColor = true;
+            // 
+            // funcRadioButton4
+            // 
+            this.funcRadioButton4.AutoSize = true;
+            this.funcRadioButton4.Location = new System.Drawing.Point(6, 106);
+            this.funcRadioButton4.Name = "funcRadioButton4";
+            this.funcRadioButton4.Size = new System.Drawing.Size(72, 17);
+            this.funcRadioButton4.TabIndex = 5;
+            this.funcRadioButton4.TabStop = true;
+            this.funcRadioButton4.Text = "Funkcja 4";
+            this.funcRadioButton4.UseVisualStyleBackColor = true;
+            // 
+            // timeTrackBar
+            // 
+            this.timeTrackBar.Location = new System.Drawing.Point(3, 177);
+            this.timeTrackBar.Name = "timeTrackBar";
+            this.timeTrackBar.Size = new System.Drawing.Size(193, 45);
+            this.timeTrackBar.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Wyświetl w zadanym momencie czasu::";
+            // 
+            // startAnimationButton
+            // 
+            this.startAnimationButton.Location = new System.Drawing.Point(3, 251);
+            this.startAnimationButton.Name = "startAnimationButton";
+            this.startAnimationButton.Size = new System.Drawing.Size(92, 34);
+            this.startAnimationButton.TabIndex = 8;
+            this.startAnimationButton.Text = "Start";
+            this.startAnimationButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Animacja:";
+            // 
+            // stopAnimationButton
+            // 
+            this.stopAnimationButton.Location = new System.Drawing.Point(104, 251);
+            this.stopAnimationButton.Name = "stopAnimationButton";
+            this.stopAnimationButton.Size = new System.Drawing.Size(92, 34);
+            this.stopAnimationButton.TabIndex = 10;
+            this.stopAnimationButton.Text = "Stop";
+            this.stopAnimationButton.UseVisualStyleBackColor = true;
+            // 
+            // saveBmpButton
+            // 
+            this.saveBmpButton.Location = new System.Drawing.Point(3, 307);
+            this.saveBmpButton.Name = "saveBmpButton";
+            this.saveBmpButton.Size = new System.Drawing.Size(92, 34);
+            this.saveBmpButton.TabIndex = 11;
+            this.saveBmpButton.Text = "Zapisz jako BMP";
+            this.saveBmpButton.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.saveBmpButton);
+            this.panel3.Controls.Add(this.funcRadioButton1);
+            this.panel3.Controls.Add(this.stopAnimationButton);
+            this.panel3.Controls.Add(this.funcRadioButton2);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.funcRadioButton3);
+            this.panel3.Controls.Add(this.startAnimationButton);
+            this.panel3.Controls.Add(this.funcRadioButton4);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.timeTrackBar);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(260, 747);
+            this.panel3.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 747);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(600, 786);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.timeTrackBar)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton funcRadioButton1;
+        private System.Windows.Forms.RadioButton funcRadioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton funcRadioButton3;
+        private System.Windows.Forms.RadioButton funcRadioButton4;
+        private System.Windows.Forms.TrackBar timeTrackBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startAnimationButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button stopAnimationButton;
+        private System.Windows.Forms.Button saveBmpButton;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
